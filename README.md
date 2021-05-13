6a1fc886bff9fd70459d11b93f3aa4d2051d45e0
高俊峰 <junfeng.gao@ingeek.com>
2021-05-13 15:44:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# a use vue frame project template
+
+## dev
+
+  npm start
+
+## build
+
+  npm run build
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>vue3 web app</h2>
+  <img src="./assets/images/vue.png"/>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App',
+})
+</script>
+<style scoped lang="less" rel="stylesheet/less">
+@import "./assets/less/app.less";
+h2 {
+  font-weight: bold;
+  text-align: center;
+  margin-top: 150px;
+}
+img{
+  display: block;
+  margin: 0 auto;
+}
+</style>
